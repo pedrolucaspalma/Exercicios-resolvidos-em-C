4640785ca80b5fa6ff5e21c9a9222eb9893bf723
Pedro Lucas Barros Palma <60949512+pedrolucaspalma@users.noreply.github.com>
2020-07-09 08:53:07 -0300

    Add files via upload

---
+++ Lista 3/ex14.c
@@ -0,0 +1,25 @@
+//Leia vários números positivos até o usuário digitar zero e, para cada número, imprima a sua raiz
+//quadrada ou uma mensagem de erro, caso não seja possível calculá-la.
+
+#include <stdio.h>
+
+int main()
+{
+    int numero, raiz, contador, resto;
+
+    numero = 1;
+
+    while(numero =! 0)
+    {
+    printf("N: ");
+    scanf("%d", &numero);
+
+    if (numero>0)
+    {
+        
+    }
+
+    
+
+    }
+}
