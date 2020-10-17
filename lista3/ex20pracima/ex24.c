f4caa108848fecccde9473afb8c136a91a48e605
Pedro Lucas Barros Palma <pedrolucaspalma@gmail.com>
2020-10-17 08:27:08 -0300

    adicionei o ex 24

--- lista3/ex20pracima/ex24.c
+++ lista3/ex20pracima/ex24.c
@@ -1,3 +1,26 @@
+#include <stdio.h>
+
 int main () {
+    
+    int n;
+    float h = 0.0;
+
+    printf("Digite N:");
+    scanf("%d",&n);
+
+    if (n <= 0){
+        printf("\nErro");
+    }
+    else{
+        
+        for(int i = 1; i <= n; i++){
+
+            h += (float) n/i;
+
+        }
+
+    }
+
+    printf("H = %.10f", h);
 
 }
